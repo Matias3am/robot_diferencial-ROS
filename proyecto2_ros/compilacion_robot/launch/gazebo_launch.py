84adfd54a15bcd151209f5a9d12cd67d909317d8
Matías González Belmar <matias.gonzalez1901@alumnos.ubiobio.cl>
2024-07-15 18:01:55 -0400

    Add files via upload

---
+++ proyecto2_ros/compilacion_robot/launch/gazebo_launch.py
@@ -0,0 +1,36 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='compilacion_robot').find('compilacion_robot')
+    default_model_path = os.path.join(pkg_share, 'urdf/robotin_xacro.urdf')
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+    
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    spawn_entity = launch_ros.actions.Node(
+    package='gazebo_ros',
+    executable='spawn_entity.py',
+    arguments=['-entity', 'robotin', '-topic', 'robot_description'],
+    output='screen'
+    )
+    
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        spawn_entity,
+    ])
